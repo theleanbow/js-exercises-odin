c6d6be77d1f59c8b37c36aefa63f58cd7272de1d
LeanBow <theleanbow@gmail.com>
2022-05-07 09:02:20 +0545

    Pass First test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringConcat='';
+for(i=0;i<num;i++){
+    stringConcat+=string;
+}
+return stringConcat;
 };
 
 // Do not edit below this line
