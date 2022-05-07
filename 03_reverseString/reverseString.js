44750311c761c61a287f7d9c069a828cce84c3f2
LeanBow <theleanbow@gmail.com>
2022-05-07 09:46:44 +0545

    Pass all test cases

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let str = string.split("");
+  let reversed = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
 };
 
 // Do not edit below this line
