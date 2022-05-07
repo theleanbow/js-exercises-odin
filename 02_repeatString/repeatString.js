d72b402842302c7087b9736f6d18ee907fb936e1
LeanBow <theleanbow@gmail.com>
2022-05-07 09:08:44 +0545

    Pass First Five test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,12 @@
-const repeatString = function(string, num) {
-    let stringConcat='';
-for(i=0;i<num;i++){
-    stringConcat+=string;
-}
-return stringConcat;
+const repeatString = function (string, num) {
+  let stringConcat = "";
+  if (num < 0) return "ERROR";
+  else {
+    for (i = 0; i < num; i++) {
+      stringConcat += string;
+    }
+    return stringConcat;
+  }
 };
 
 // Do not edit below this line
