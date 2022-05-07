6396d019881d8990d11bad592cc6eb90b8def435
LeanBow <theleanbow@gmail.com>
2022-05-07 13:12:52 +0545

    Pass all test cases

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 4 == 0) {
+    if (year % 25 == 0) {
+      if (year % 400 == 0) return true;
+      else return false;
+    } else return true;
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
