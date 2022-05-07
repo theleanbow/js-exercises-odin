dff4a6c821c97ff208e2fcc4abdbb5b00036729e
LeanBow <theleanbow@gmail.com>
2022-05-07 13:25:25 +0545

    Pass all test cases

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+  return parseFloat(((5 * (fahrenheit - 32)) / 9).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (celcius) {
+  return parseFloat(((9 * celcius) / 5 + 32).toFixed(1));
 };
-
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
